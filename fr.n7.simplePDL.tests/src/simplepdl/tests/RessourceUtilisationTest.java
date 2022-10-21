a7e720720ce0f1abc94e2575ea6f4d33650d64cc
MarinFranot <116350926+MarinFranot@users.noreply.github.com>
2022-10-21 12:51:51 +0200

    Add files via upload

---
+++ fr.n7.simplePDL.tests/src/simplepdl/tests/RessourceUtilisationTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package simplepdl.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import simplepdl.RessourceUtilisation;
+import simplepdl.SimplepdlFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Ressource Utilisation</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class RessourceUtilisationTest extends TestCase {
+
+	/**
+	 * The fixture for this Ressource Utilisation test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected RessourceUtilisation fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(RessourceUtilisationTest.class);
+	}
+
+	/**
+	 * Constructs a new Ressource Utilisation test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public RessourceUtilisationTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Ressource Utilisation test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(RessourceUtilisation fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Ressource Utilisation test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected RessourceUtilisation getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(SimplepdlFactory.eINSTANCE.createRessourceUtilisation());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //RessourceUtilisationTest
